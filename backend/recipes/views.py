8e8c5b429a8777e8fe86632123ca78aa03eb37da
Никита Лощинин <terasentplay@yandex.ru>
2024-01-26 20:11:53 +0700

    delete unusual imports

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,10 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from django.http import Http404
 from django.shortcuts import get_object_or_404
 from .models import Recipe, Product, RecipeProduct
-from .serializers import RecipeSerializer
 from django.shortcuts import render
 
 
