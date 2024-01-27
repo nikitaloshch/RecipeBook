4afb5b45858b2c780fa2b867fc5eba4b67b8fbe3
Никита Лощинин <terasentplay@yandex.ru>
2024-01-27 22:53:27 +0700

    Fix bugs, optimize code

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,10 +1,10 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
+from django.db.models import Sum, Case, When, IntegerField
+from django.db import transaction
 from .models import Recipe, Product, RecipeProduct
-from .serializers import RecipeSerializer
-from django.shortcuts import render
 
 
 @api_view(['GET'])
@@ -17,23 +17,32 @@ def add_product_to_recipe(request, recipe_id, product_id, weight):
     recipe = get_object_or_404(Recipe, id=recipe_id)
     product = get_object_or_404(Product, id=product_id)
 
-    recipe_product, created = RecipeProduct.objects.get_or_create(recipe=recipe, product=product, defaults={'weight': weight})
+    with transaction.atomic():
+        recipe_product, created = RecipeProduct.objects.get_or_create(
+            recipe=recipe, product=product, defaults={'weight': weight})
 
-    if not created:
-        recipe_product.weight = weight
-        recipe_product.save()
+        if not created:
+            recipe_product.weight = weight
+            recipe_product.save()
 
-    return Response({'status': 'success', 'message': 'Product added to recipe'}, status=status.HTTP_200_OK)
+    return Response({
+            'status': 'success',
+            'message': f'Product {product.name} {weight}g added to recipe {recipe.name}'}, status=status.HTTP_200_OK)
 
 
 @api_view(['GET'])
+@transaction.atomic
 def cook_recipe(request, recipe_id):
     recipe = get_object_or_404(Recipe, id=recipe_id)
     products = recipe.products.all()
 
-    for product in products:
-        product.cooked += 1
-        product.save()
+    try:
+        with transaction.atomic():
+            for product in products:
+                product.cooked += 1
+                product.save()
+    except Exception as e:
+        return Response({'status': 'error', 'message': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     return Response({'status': 'success', 'message': f'Recipe cooked: {recipe.name}'}, status=status.HTTP_200_OK)
 
@@ -41,9 +50,18 @@ def cook_recipe(request, recipe_id):
 @api_view(['GET'])
 def show_recipes_without_product(request, product_id):
     product = get_object_or_404(Product, id=product_id)
-    recipes_without_product = Recipe.objects.exclude(recipeproduct__product__id=product_id).distinct()
 
-    serializer = RecipeSerializer(recipes_without_product, many=True)
+    recipes_without_product = (
+        Recipe.objects
+        .annotate(product_weight=Sum(
+            Case(
+                When(recipeproduct__product=product, then='recipeproduct__weight'),
+                default=0,
+                output_field=IntegerField()
+            )
+        ))
+        .exclude(product_weight__gte=10)
+        .distinct()
+    )
 
-    return render(request, 'show_recipes_without_product.html', {'recipes': serializer.data})
-
+    return render(request, 'show_recipes_without_product.html', {'recipes': recipes_without_product})
